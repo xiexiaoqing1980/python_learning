064e41538a2178786a5959f82c0460d16f742d00
xinna <1805744514@qq.com>
2019-09-04 15:31:34 +0800

    commit new files

--- crawl_kingdee_2/TestCase/LoginTest.py
+++ crawl_kingdee_2/TestCase/LoginTest.py
@@ -1,6 +1,6 @@
 
 import unittest
-import crawl_kingdee_2.loginPage
+import crawl_kingdee_2.loginPage as loginPage
 from  selenium import webdriver
 
 class loginTest(unittest.TestCase):
@@ -14,5 +14,25 @@ class loginTest(unittest.TestCase):
         self.driver  = webdriver.Chrome(executable_path=chromepath, chrome_options=chrome_options)
         self.base_url="https://ierp.kingdee.com:2024/biz15/index.html"
 
-    def test_input_uername(self):
-        login_page=loginPage(self.driver,self.base_url,)
+    def login_fail(self):
+        self._login("")
+        assert()
+
+
+    def login_success(self):
+        login_page=loginPage(self.driver,self.base_url,"金蝶云苍穹")
+        login_page.open()
+        login_page.input_username("18826135235")
+        login_page.input_password("1234567")
+        login_page.submit_button()
+
+    def _login(self,username,password):
+        login_page = loginPage(self.driver, self.base_url, "金蝶云苍穹")
+        login_page.open()
+        login_page.input_username(username)
+        login_page.input_password(password)
+        login_page.submit_button()
+
+
+
+
